3fddd0c95bbbbdb729a30921957cdea2a22a3e86
wonbinjeong <jeongwonbin0503@gmail.com>
2022-02-25 07:13:03 -0700

    refactor: moved more constant values

--- ui-service/src/util/constants.js
+++ ui-service/src/util/constants.js
@@ -2,42 +2,60 @@ export const Assets = [
     {
       fundType : "Cash and Investments",
       fund : [
-        {name:"Chequing", amount: 2000.00},
-        {name:"Savings for Taxes", amount: 4000.00},
-        {name:"Rainy Day Fund", amount: 506.00},
-        {name:"Savings for Fun", amount: 5000.00},
-        {name:"Savings for Travel", amount: 400.00},
-        {name:"Savings for Personal Development", amount: 200.00},
-        {name:"Investment 1", amount: 5000.00},
-        {name:"Investment 2", amount: 60000.00},
-        {name:"Investment 3", amount: 24000.00}
+        {name:"Chequing", amount: 2000},
+        {name:"Savings for Taxes", amount: 4000},
+        {name:"Rainy Day Fund", amount: 506},
+        {name:"Savings for Fun", amount: 5000},
+        {name:"Savings for Travel", amount: 400},
+        {name:"Savings for Personal Development", amount: 200},
+        {name:"Investment 1", amount: 5000},
+        {name:"Investment 2", amount: 60000},
+        {name:"Investment 3", amount: 24000}
       ]
     },
     {
       fundType: "Long Term Assets",
       fund: [
-        {name:"Primary Home", amount: 455000.00},
-        {name:"Second Home", amount: 1564321.00},
-        {name:"Other", amount: 0.00},
+        {name:"Primary Home", amount: 455000},
+        {name:"Second Home", amount: 1564321},
+        {name:"Other", amount: 0},
       ]
     }
   ]
   
 export const Liabilities = [
-{
-    fundType: "Short Term Liabilities",
-    fund: [
-    {name:"Credit Card 1", amount: 4342.00},
-    {name:"Credit Card 2", amount: 322.00}
-    ]
-},
-{
-    fundType: "Long Term Debt",
-    fund: [
-    {name:"Mortgage 1", amount: 250999.00},
-    {name:"Mortgage 2", amount: 632634.00},
-    {name:"Line of Credit", amount: 10000.00},
-    {name:"Investment Loan", amount: 10000.00},
-    ]
-}
+    {
+        fundType: "Short Term Liabilities",
+        fund: [
+        {name:"Credit Card 1", amount: 4342},
+        {name:"Credit Card 2", amount: 322}
+        ]
+    },
+    {
+        fundType: "Long Term Debt",
+        fund: [
+        {name:"Mortgage 1", amount: 250999},
+        {name:"Mortgage 2", amount: 632634},
+        {name:"Line of Credit", amount: 10000},
+        {name:"Investment Loan", amount: 10000},
+        ]
+    }
 ]
+
+export const Currencies = [
+    "CAD", "USD", "KRW", "JPY", "GBP", 
+    "EUR", "CHF", "ZAR", "AUD", "CNY"
+];
+
+export const CurrencySymbols = {
+    "CAD" : "C$",
+    "USD" : "$",
+    "KRW" : "₩",
+    "JPY" : "¥",
+    "GBP" : "£",
+    "EUR" : "€",
+    "CHF" : "CHf",
+    "ZAR" : "R",
+    "AUD" : "A$",
+    "CNY" : "C¥",
+}
